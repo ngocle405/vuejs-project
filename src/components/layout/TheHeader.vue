158d92dbc86cce958215f56d4b87c47433fb7af8
Your Name <you@example.com>
2021-12-03 22:56:32 +0700

    commit all

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="m-header">
+            <div class="m-header-left">
+                <select name="" id="" class="m-dropdown">
+                    <option value="">CÔNG TY TNHH - SẢN XUẤT - DỊCH VỤ QUI PHÚC</option>
+                    <option value="">Nhà hàng Sen Hồ</option>
+                </select>
+
+            </div>
+            <div class="m-header-right">
+                <img class="m-user-avt" style="border-radius: 50px;" src="/../assets/default-avatar.jpg" alt="">
+                <div class="m-user-name">Lê Thanh Ngọc</div>
+                <div class="m-icon-option">
+                    <img src="../../assets/img/down.png" style="width:15px;margin-top: 5px;" alt="">
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url("../../assets/fonts/fontawesome-5.15.1/css/all.min.css");
+@import url("../../style/main.css");
+</style>
---
+++ src/components/layout/TheNavBar.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="m-navbar">
+        <div class="m-nav-top">
+            <div class="m-toggle">
+                <i class="fas fa-th"></i>
+            </div>
+            <div class="m-logo">
+                <img src="../../assets/img/Logo_Module_TiengViet_White.66947422.svg" alt="">
+            </div>
+        </div>
+        <div class="m-menu">
+            <ul>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-chart-pie"></i>
+                        </div>
+                        <div class="m-name-submenu">Tổng quan</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-archive"></i>
+                        </div>
+                        <div class="m-name-submenu">Tiền mặt</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-university"></i>
+                        </div>
+                        <div class="m-name-submenu">Tiền gửi</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-shopping-bag"></i>
+                        </div>
+                        <div class="m-name-submenu">Mua hàng</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-shopping-cart"></i>
+                        </div>
+                        <div class="m-name-submenu">Bán hàng</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="far fa-file-alt"></i>
+                        </div>
+                        <div class="m-name-submenu">Quản lý hóa đơn</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-warehouse"></i>
+                        </div>
+                        <div class="m-name-submenu">Kho</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-tools"></i>
+                        </div>
+                        <div class="m-name-submenu">Công cụ dụng cụ</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-car"></i>
+                        </div>
+                        <div class="m-name-submenu">Tài sản cố định</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-university"></i>
+                        </div>
+                        <div class="m-name-submenu">Thuế</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-file-signature"></i>
+                        </div>
+                        <div class="m-name-submenu">Giá thành</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-address-book"></i>
+                        </div>
+                        <div class="m-name-submenu">Tổng hợp</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-yen-sign"></i>
+                        </div>
+                        <div class="m-name-submenu">Ngân sách</div>
+                    </a>
+                </li>
+                <li class="">
+                    <a href="/page/employee.html">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-chart-bar"></i>
+                        </div>
+                        <div class="m-name-submenu">Báo cáo</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="m-icon-submenu">
+                            <i class="fas fa-chart-line"></i>
+                        </div>
+                        <div class="m-name-submenu">Phân tích tài chính</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url("../../assets/fonts/fontawesome-5.15.1/css/all.min.css");
+@import url("../../style/main.css");
+</style>
